dc4e487dd2ce4fd2cc2cbd2b924b20952028f6a8
Marat <maretcoop11@gmail.com>
2020-02-19 15:34:20 +0300

    Lab1/Generator

---
+++ Lab1/generator.py
@@ -0,0 +1,79 @@
+import random
+import string
+
+
+class Generator:
+
+    def __init__(self, num=1000000):
+        self._num = num
+        self._f = open('addresses_1.txt', 'w')
+
+    def rand(self):
+        return random.random()
+
+    def __del__(self):
+        self._f.close()
+
+    def generateFile(self):
+        for i in range(self._num):
+            address = self.mailto()+' '+self.name()+self.dogSign()+self.generateString()+self.dot() + \
+                      self.generateString() + self.text()+'\n'
+            self._f.write(address)
+
+    def generateString(self, a=-5, b=80):
+        letters = random.randint(a, b)
+        r = self.rand()
+        if r < 0.33:
+            return ''.join(random.choices(string.ascii_letters + string.digits, k=letters))
+        elif r < 0.66:
+            return ''.join(random.choices(string.ascii_letters, k=letters))
+        else:
+            return ''.join(random.choices(string.digits, k=letters))
+
+    def mailto(self):
+        if self.rand() > 0.4:
+            return 'mailto:'
+        else:
+            return ''
+
+    def name(self):
+        name = ['n', 'a', 'm', 'e']
+        random.shuffle(name)
+        return ''.join(name)+''.join(random.choices(string.digits, k=random.randint(-5,5)))
+
+    def dogSign(self):
+        if self.rand() > 0.4:
+            return '@'
+        else:
+            return ''
+
+    def dot(self):
+        if self.rand() > 0.2:
+            return '.'
+        else:
+            return ''
+
+    def text(self):
+        text = self.generateString()
+        if len(text) != 0:
+            r = self.rand()
+            if r > 0.3:
+                text = '?subject='+text
+                return text
+            else:
+                return text
+        else:
+            return ''
+
+
+if __name__ == '__main__':
+    #gen = Generator()
+    #gen.generateFile()
+    '''
+    f = open('addresses.txt', 'r')
+    add = f.read()
+    addr = add.split('\n')
+    print(len(addr))
+    print(addr[999999])
+    '''
+    #print(gen.name())
